68ba20926dcc0dd02098a2902847cf03c6ae285f
semrec1 <120269292+semrec1@users.noreply.github.com>
2022-12-10 22:14:13 +0530

    Create Rcalc.py

---
+++ Rcalc.py
@@ -0,0 +1,22 @@
+# i.Using with and without R objects on console 
+# using without objects
+2587+2149
+123*344
+50/2
+
+# using with objects
+A= 100
+B= 200
+C= B-A
+C
+# ii.Using mathematical functions on console
+a= 100
+class(a)
+b= 500
+class(b)
+sum(a-b)
+
+# iii.Write an R script, to create R objects for calculator application and save in a specified location in disk. 
+getwd()
+a=100
+write.csv(a,'a.csv')
